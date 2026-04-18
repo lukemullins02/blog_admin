890b6024fd51f14fb96c0bf586661f0466ba6f59
= <lukejmullins@gmail.com>
2026-04-18 09:41:37 -0400

    Add reducer function

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,20 +1,65 @@
-import { useState, useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import { deletePost, getPosts, putPublish } from "../services/postService";
 import Post from "./templates/Post";
 import Navbar from "./Navbar";
 
+const postReducer = (state, action) => {
+  console.log(state.posts);
+
+  switch (action.type) {
+    case "FETCH_POSTS_START":
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+
+    case "FETCH_POSTS_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        posts: action.payload,
+      };
+    case "FETCH_POSTS_ERROR":
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    case "DELETE_POST":
+      return {
+        ...state,
+        posts: state.posts.filter((post) => post.id !== action.payload),
+      };
+    case "TOGGLE_PUBLISH_POST":
+      return {
+        ...state,
+        posts: state.posts.map((post) =>
+          post.id === action.payload
+            ? { ...post, isPublished: !post.isPublished }
+            : post,
+        ),
+      };
+    default:
+      return state;
+  }
+};
+
 function Posts() {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(postReducer, {
+    posts: [],
+    loading: false,
+    error: null,
+  });
 
   const handleStatus = async (postid, published) => {
     try {
       await putPublish(postid, { published: !published });
 
-      const response = await getPosts();
-
-      setPosts([...response]);
+      dispatch({
+        type: "TOGGLE_PUBLISH_POST",
+        payload: postid,
+      });
     } catch (err) {
       if (err.response) {
         console.log(err.response.data.message);
@@ -33,9 +78,10 @@ function Posts() {
       if (userInput.toLowerCase() === "yes") {
         await deletePost(postid);
 
-        const response = await getPosts();
-
-        setPosts([...response]);
+        dispatch({
+          type: "DELETE_POST",
+          payload: postid,
+        });
       }
     } catch (err) {
       if (err.response) {
@@ -47,30 +93,40 @@ function Posts() {
   };
 
   useEffect(() => {
+    dispatch({
+      type: "FETCH_POSTS_START",
+    });
+
     const fetchPosts = async () => {
       try {
         const response = await getPosts();
 
-        setPosts(response);
+        dispatch({
+          type: "FETCH_POSTS_SUCCESS",
+          payload: response,
+        });
       } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
+        dispatch({
+          type: "FETCH_POSTS_ERROR",
+          payload: err.message,
+        });
       }
     };
 
     fetchPosts();
   }, []);
 
-  if (loading) return <p className="text-white text-xl pl-4">Loading...</p>;
-  if (error) return <p className="text-red-600 text-xl pl-4">Error: {error}</p>;
+  if (state.loading)
+    return <p className="text-white text-xl pl-4">Loading...</p>;
+  if (state.error)
+    return <p className="text-red-600 text-xl pl-4">Error: {state.error}</p>;
   return (
     <div className="min-h-screen w-screen  bg-[#1d3557]">
       <Navbar />
       <h1 className="text-white text-5xl mt-4 text-center ">Articles</h1>
 
       <ul className="w-full flex flex-col items-center mt-7">
-        {posts.map((post) => (
+        {state.posts.map((post) => (
           <Post
             key={post.id}
             post={post}
